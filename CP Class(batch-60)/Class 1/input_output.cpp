16ff79077e5133f864f4b4fae4540cef49de1bb9
Shakil Ahamed Shuvo <100990764+Shuv00@users.noreply.github.com>
2023-09-26 10:11:43 +0600

    Add files via upload

---
+++ CP Class(batch-60)/Class 1/input_output.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<n<<endl;
+    //comment: here cin for input , cout for showing the output and the endl for new line
+}
---
+++ CP Class(batch-60)/class 2/if_else_maximum_number.cpp
@@ -0,0 +1,18 @@
+// find the largest number between two number
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    cin>>a>>b;
+    if(a>=b){
+        cout<<"max = "<<a<<endl;
+    }
+    else {
+        cout<<"max = "<<b<<endl;
+    }
+}
+
+
+
+
+
